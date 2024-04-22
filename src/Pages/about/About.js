7dbc170e9cfafff85a2d5c3fbc06230aea60004b
Yanis Lartigue <lartigueyanis@yahoo.com>
2024-04-22 23:32:48 +0200

    ajout de collapse dans about

--- src/Pages/about/About.js
+++ src/Pages/about/About.js
@@ -2,6 +2,8 @@ import React from "react";
 import Header from "../../components/header/Header";
 import Banner from "../../components/Banner/Banner";
 import ImageaboutBanner from "../../assets/banner/aboutBanner.png";
+import Collapse from "../../components/collapse/Collapse";
+import CollapseData  from "../../Datas/collapses.json";
 import Footer from "../../components/footer/Footer";
 
 export default function About() {
@@ -10,6 +12,17 @@ export default function About() {
       <Header />
       <main>
         <Banner image={ImageaboutBanner} />
+        <div className="collapse">
+          <div className="collapse__dropdown">
+            {CollapseData.map((item) => {
+              return (
+                <div key={item.id}>
+                  <Collapse content={item.content} title={item.title} />
+                </div>
+              );
+            })}
+          </div>
+        </div>
       </main>
       <footer>
         <Footer />
