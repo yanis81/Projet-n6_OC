d7915ad383f6d961e584aea0184814b432872a16
Yanis Lartigue <lartigueyanis@yahoo.com>
2024-05-15 09:36:45 +0200

    transition collapse

--- src/components/collapse/Collapse.js
+++ src/components/collapse/Collapse.js
@@ -11,23 +11,26 @@ const Collapse = ({ title, content }) => {
 
   return (
     // affiche le collapse replié par défaut et l'ouvre au clic puis le referme au clic en faisant disparaitre le texte et le style
-    <div className="collapse__dropdown__container">
-      <div className="collapse__dropdown__title">
-        <h2>{title}</h2>
-        <p onClick={display}>     
-           {isOpen ? 
-          //  si l'état est true alors tu joue rotated sinon non
-             ( <i className="fa-solid fa-chevron-down rotated"></i>)
-             : 
-             ( <i className="fa-solid fa-chevron-down"></i>)
-            }       
-         </p>
-      </div>
-      {/* Si le collapse est à TRUE alors il affichera la description */}
-      <div className="collapse__dropdown__content">
-        {isOpen && <div>{content}</div>}
+    <div className="collapse__dropdown">
+        <div className="collapse__dropdown__container">
+          <div className="collapse__dropdown__title">
+            <h2>{title}</h2>
+            <p onClick={display}>     
+              {isOpen ? 
+              //  si l'état est true alors tu joue rotated sinon non
+                ( <i className="fa-solid fa-chevron-down rotated"></i>)
+                : 
+                ( <i className="fa-solid fa-chevron-down"></i>)
+                }       
+            </p>
+          </div>
+          {/* Si le collapse est à TRUE alors il affichera la description */}
+          <div className={`${isOpen?'opened':''} collapse__dropdown__content`}>  {/* style css apporter dynamiquement */}
+            {isOpen && <div className="incontent">{content}</div>}
+          </div>
       </div>
     </div>
+    
   );
 };
 
